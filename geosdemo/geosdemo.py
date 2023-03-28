61f97347ec39354ea83b7b4f05297375bbba6cee
Qiusheng Wu <giswqs@gmail.com>
2023-03-28 10:43:12 -0400

    Added two random functions

--- geosdemo/geosdemo.py
+++ geosdemo/geosdemo.py
@@ -1,1 +1,20 @@
 """Main module."""
+
+import string
+import random
+
+def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
+    letters = string.ascii_lowercase
+    if upper:
+        letters += string.ascii_uppercase
+    if digits:
+        letters += string.digits
+    if punctuation:
+        letters += string.punctuation
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def generate_lucky_number(length=1):
+    result_str = ''.join(random.choice(string.digits) for i in range(length))
+    return int(result_str)
