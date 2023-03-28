3e890ee01f82b7240a1d419c83fac965131f013e
Qiusheng Wu <giswqs@gmail.com>
2023-03-28 10:55:00 -0400

    Add docstrings

--- geosdemo/geosdemo.py
+++ geosdemo/geosdemo.py
@@ -4,6 +4,18 @@ import string
 import random
 
 def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
+    """Generates a random string of a given length.
+
+    Args:
+        length (int, optional): The length of the string to generate. Defaults to 10.
+        upper (bool, optional): Whether to include uppercase letters. Defaults to False.
+        digits (bool, optional): Whether to include digits. Defaults to False.
+        punctuation (bool, optional): Whether to include punctuation. Defaults to False.
+
+    Returns:
+        str: The generated string.
+    """
+
     letters = string.ascii_lowercase
     if upper:
         letters += string.ascii_uppercase
@@ -16,5 +28,14 @@ def generate_random_string(length=10, upper=False, digits=False, punctuation=Fal
 
 
 def generate_lucky_number(length=1):
+    """Generates a random number of a given length.
+
+    Args:
+        length (int, optional): The length of the number to generate. Defaults to 1.
+
+    Returns:
+        int: The generated number.
+    """
+
     result_str = ''.join(random.choice(string.digits) for i in range(length))
     return int(result_str)
